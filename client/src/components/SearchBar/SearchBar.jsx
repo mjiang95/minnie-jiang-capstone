5eabafa5572eba55d981605465dece0a9305470b
mjiang95 <minniejiang2013@gmail.com>
2021-12-04 23:11:01 -0800

    update search bar

---
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import "./SearchBar.scss";
+import searchIcon from "../../assets/icons/search_icon_152764.png";
+import { Link } from "react-router-dom";
+import { Component } from "react";
+const axios = require("axios");
+
+
+class SearchBar extends Component {
+  state = {
+    heroSearchResults: [], 
+    filteredData: [],
+    setFilteredData: []
+  };
+
+  componentDidMount() {
+    axios.get(`http://localhost:8080/api/marvel/`).then((response) => {
+      this.setState({
+        heroSearchResults: response.data,
+      });
+      console.log(this.state.heroSearchResults);
+    });
+  }
+
+//  handleFilter = (e) => {
+//   const searchWord = e.target.value
+//   const newFilter = this.state.heroSearchResults.filter((value) => {
+//     return value.name.includes(searchWord);
+//   })
+//   this.setState({
+//     filteredData: newFilter
+//   })
+// }
+
+  render() {
+    const filterHeroArray = this.state.filteredData;
+    const heroSearchArray = this.state.heroSearchResults;
+
+    const handleFilter = (e) => {
+      const searchWord = e.target.value
+      const newFilter = heroSearchArray.filter((value) => {
+        return value.name.includes(searchWord);
+      })
+      this.setState({
+        filteredData: newFilter
+      })
+    }
+
+    return (
+      <>
+        <div className="search-bar__wrapper">
+          <div className="search-input">
+          <input className="input" type="text" placeholder= "Type to search..." onChange={handleFilter}/>
+          <div className="autocomplete-box">
+            {filterHeroArray.map((results) => {
+              return (
+                <>
+                <ul className = "itemslist">
+                    <Link to={`/character/${results.id}`}>
+                      <li className= "items">{results.name}</li>
+                    </Link>
+                  </ul>
+                </>
+              );
+            })}
+          </div>
+          <button className="search-button">
+            <img className="search-icon" src={searchIcon}></img>
+          </button>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default SearchBar;
